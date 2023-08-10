c82530978d20854ca499b87be120b6cc69a53912
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-10 09:17:57 -0700

    Added 4.Hello,puts Script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program demonstrates the use of the puts() function
+ *              to print a message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*  prints exactly "Programming is like building a multilingual puzzle */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
