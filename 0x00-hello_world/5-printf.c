11220431e1c98da51f05b61edd28c91909d012a3
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-10 09:40:31 -0700

    Added 5.Hello,printf Script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program demonstrates the use of the printf() function
+ *              to print a message to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* prints exactly with proper grammar, but the outcome is a piece of art, */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
