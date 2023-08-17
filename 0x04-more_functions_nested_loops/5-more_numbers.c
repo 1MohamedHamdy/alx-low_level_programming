8e16edeb6495a5a0415218cd116fe5b416bb89a3
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-17 20:09:29 +0300

    Added Task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+int i = 0;
+int j = 0;
+int counter = 0;
+const int x = 1;
+int flag = 0;
+for (; i < 10; i++)
+{
+for (; j < 15; j++)
+{
+if (j>9 || flag == 1)
+{
+flag = 1;
+counter++;
+if(counter == 1)
+j = 0;
+if(counter ==6)break;
+_putchar(x + '0');
+}
+_putchar(j + '0');
+}
+j = 0 ;
+counter = 0 ;
+flag = 0 ;
+_putchar('\n');
+}
+}
