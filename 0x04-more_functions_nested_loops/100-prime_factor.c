57defc6df02ad639cc68b9def5b02b6c5018a9a1
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-18 00:22:17 +0300

    Added Task11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-/**
- * _sqrt - Calculate the square root of a number using
- * the Newton-Raphson method.
- * @x: The number for which to calculate the square root.
- * Return: The calculated square root.
- */
-double _sqrt(double x)
-{
-float sqrt, tmp;
-sqrt = x / 2;
-tmp = 0;
-while (sqrt != tmp)
-{
-tmp = sqrt;
-sqrt = (x / tmp + tmp) / 2;
-}
-return (sqrt);
-}
-
-/**
- * largest_prime_factor - Find and print the largest prime factor of a number.
- * @n: The number for which to find the largest prime factor.
- */
-void largest_prime_factor(long int n)
-{
-int largest_prime, prmNu;
-while (n % 2 == 0)
-n = n / 2;
-for (prmNu = 3; prmNu <= _sqrt(n); prmNu += 2)
-{
-while (n % prmNu == 0)
-{
-n = n / prmNu;
-largest_prime = prmNu;
-}
-}
-if (n > 2)
-largest_prime = n;
-printf("%d\n", largest_prime);
-}
-
 
 /**
  * main - Entry point of the program.
@@ -50,6 +7,19 @@ printf("%d\n", largest_prime);
  */
 int main(void)
 {
-largest_prime_factor(612852475143);
+unsigned long int num = 612852475143UL;
+unsigned long int divisor = 2;
+while (num > 1)
+{
+if (num % divisor == 0)
+{
+num /= divisor;
+}
+else
+{
+divisor++;
+}
+}
+printf("%lu\n", divisor);
 return (0);
 }
