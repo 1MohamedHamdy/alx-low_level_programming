3a41297693e95961adcd612cd6ad04afe98d78bb
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-17 22:25:43 +0300

    Added Task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MIN_NUM 1
+#define MAX_NUM 100
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = MIN_NUM; i <= MAX_NUM; i++)
+{
+if (i % 3 == 0)
+{
+if (i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("Fizz ");
+}
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+printf("\n");
+return (0);
+}
