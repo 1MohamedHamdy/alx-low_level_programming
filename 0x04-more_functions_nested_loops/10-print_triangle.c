30ff277a60c189de821ae81a67efdff1df889e60
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-17 23:03:02 +0300

    Added Task10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
+/**
+ * print_triangle - Prints a triangle pattern of '#' characters.
+ * @size: The size of the triangle's height.
+ */
 void print_triangle(int size)
 {
-    int i;
-    int j;
-    if (size <= 0)
-        _putchar('\n');
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            if (j >= size - i - 1)
-                _putchar('#');
-            else 
-                _putchar(' ');    
-        }
-        _putchar('\n');
-    }    
+int i;
+int j;
+if (size <= 0)
+_putchar('\n');
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (j >= size - i - 1)
+_putchar('#');
+else
+_putchar(' ');
+}
+_putchar('\n');
+}
 }
