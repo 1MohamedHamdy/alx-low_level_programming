688f56991f74c3cde377ac07cc16a461be266a81
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-21 13:19:02 -0700

    Added Task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,18 @@
  */
 void puts_half(char *str)
 {
-	int i, j = 0, length, strlen = 0;
+	int i, j = 0, start, strlen = 0;
 
 	while (str[j] != '\0')
 	{
 		strlen++;
 		j++;
 	}
-	length = strlen / 2;
-	for (i = length; i < strlen; i++)
+	if (strlen % 2 == 0)
+		start = strlen / 2;
+	else
+		start = (strlen + 1) / 2;
+	for (i = start; i < strlen; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
