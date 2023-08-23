3235ca36c6362b34452867a3b32bc4e18b99327c
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-23 13:07:26 -0700

    Added Task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, j = 0;
-        char tmp;
+	int i, j = 0, temp;
 
-        for (i = n-1, j = 0 ; i >= n / 2 && j <= n / 2; i--, j++)
-        {
-                tmp = a[j];
-                a[j] = a[i];
-                a[i] = tmp;
-        }
+	for (i = n - 1, j = 0 ; i >= n / 2 && j <= n / 2; i--, j++)
+	{
+		temp = a[j];
+		a[j] = a[i];
+		a[i] = temp;
+	}
 }
