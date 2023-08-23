7177c960e1ed145ecfa90b80dda3d205ae039b60
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-08-23 16:44:36 -0700

    Added Task13

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,37 +8,35 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int j, k, l;
 
 	if (size <= 0)
-	{
 		printf("\n");
-		return;
-	}
-	for (i = 0; i < size; i += 10)
+	else
 	{
-		printf("%08x: ", i);
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < size; j += 10)
 		{
-			if (i + j < size)
-				printf("%02x", (unsigned char)b[i + j]);
-			else
-				printf("  ");
-			if (j % 2 == 1)
-				printf(" ");
-		}
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
 			{
-				if (b[i + j] >= 32 && b[i + j] <= 126)
-					printf("%c", b[i + j]);
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
 				else
-					printf(".");
+					printf("  ");
 			}
-			else
-				printf(" ");
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
