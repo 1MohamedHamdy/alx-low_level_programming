cc9a5c88027d0d3fa5c7142eae4372cb9fac92d4
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-09-07 16:03:48 -0700

    Added Task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,32 +14,31 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *ptr_new = NULL;
 	unsigned int i, min_size = old_size;
 
-	ptr_new = malloc(new_size);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	if (ptr == NULL)
 	{
+		ptr_new = malloc(new_size);
+		if (ptr_new == NULL)
+			return (NULL);
 		return (ptr_new);
 	}
-	else
+	if (old_size == new_size)
+		return (ptr);
+	ptr_new = malloc(new_size);
+
+	if (ptr_new == NULL)
+		return (NULL);
+	if (old_size > new_size)
+		min_size = new_size;
+	for (i = 0; i < min_size; i++)
 	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		else if (old_size > new_size)
-		{
-			min_size = new_size;
-		}
-		else if (old_size == new_size)
-		{
-			return (ptr);
-		}
-		for (i = 0; i < min_size; i++)
-		{
-			*((char *)ptr_new + i) = *((char *)ptr + i);
-		}
-		free(ptr);
+		*((char *)ptr_new + i) = *((char *)ptr + i);
 	}
+	free(ptr);
 	return (ptr_new);
 }
