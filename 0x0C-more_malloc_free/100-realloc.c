f3f5e15cadf2f6d88e0ddc52f1aeb46ef0c9f119
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-09-07 16:11:16 -0700

    Added Task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *ptr_new = NULL;
 	unsigned int i, min_size = old_size;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
