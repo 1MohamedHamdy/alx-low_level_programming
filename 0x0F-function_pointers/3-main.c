4ebd7eeda30e26f0c5c6b49b5c805eb5ed54b14a
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-09-13 14:00:11 -0700

    Added Task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 
 	operator = get_op_func(argv[2]);
-	if (operation == NULL)
+	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
