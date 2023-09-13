3c8ae926b0840f07c8aa5ab59f38f0d36cb03370
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-09-13 15:12:46 -0700

    Added Task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		return (98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	if (operator == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		return (99);
 	}
 	result = operation(num1, num2);
 	printf("%d\n", result);
