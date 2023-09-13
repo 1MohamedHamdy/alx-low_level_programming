87c4b4148acfdf64468f5872ad5a33356aeb9a5d
1MohamedHamdy <mohamedhamdyabdelsamie@gmail.com>
2023-09-13 15:23:48 -0700

    Added Task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (*(ops[i].op) == *s && s[1] == '\0')
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
+    
 	return (NULL);
 }
